a9b6030813030d303b6c2db69567e310ac9bd1be
esterfeliciano <ester.felicianomelo@gmail.com>
2026-07-17 11:25:04 -0300

    subuindo questão 1 respondida

--- lista6/resul1.py
+++ lista6/resul1.py
@@ -31,3 +31,42 @@
 ## - Exemplo 2: a = 3, b = 4, c = 5 -> "Escaleno"
 ## - Exemplo 3: a = 1, b = 2, c = 3 -> "Não é um triângulo"
 ## - Exemplo 4: a = 2, b = 2, c = 3 -> "Isósceles"
+
+a =int(input('Digite o lado a do triâgulo: '))
+b =int(input('Digite o lado b do triângilo: '))
+c =int(input('Digite o lado c do triângulo :'))
+
+## conferindo o lado a
+if a < b + c:
+    ladoA = True
+else:
+    ladoA = False
+
+## conferindo o lado b
+if b < a + c:
+    ladoB = True
+else:
+    ladoB = False
+
+## conferindo o lado c
+if c < a + b:
+    ladoC = True
+else:
+    ladoC = False
+
+## vendo se é um triangulo válido
+if ladoA == True and ladoB == True and ladoC == True:
+   ladosV = True
+else:
+    ladosV = False
+
+## classificando só se for um triângulo válido
+if ladosV == True:
+    if a == b and b == c:
+        print('O triângulo é equilátero.')
+    elif a == b or a == c or b == c:
+        print('O triângulo é isóceles.')
+    else:
+        print('O triâgulo é escaleno.')
+else:
+    print('Não é um triângulo.')
